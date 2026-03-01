6731cc4d78be0f0f08ed560e437724a6da3aaf2f
Marina Felix <maarinafelixf@gmail.com>
2026-03-01 13:12:06 -0300

    Atualização do Portfolio

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-const botao = document.getElementById('botao-tema');
-const body = document.body;
+const toggleBtn = document.getElementById("theme-toggle");
 
-// Persistência do tema
-const temasalvo = localStorage.getItem('tema');
-temaEscuro(temasalvo === 'escuro');
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
 
-// Função para alternar entre tema claro e escuro
-function temaEscuro(tipo) {
-  if (tipo == true) {
-    body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  if (document.body.classList.contains("light-mode")) {
+    toggleBtn.textContent = "☀️";
   } else {
-    body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    toggleBtn.textContent = "🌙";
   }
-}
-
-botao.addEventListener('click', () => {
-  const isescuro = body.classList.toggle('escuro');
-  temaEscuro(isescuro);
-  localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
 });
 
-// Scroll suave para links de navegação
-const navLinks = document.querySelectorAll('#menu ul a.link');
-navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      const headerHeight = document.querySelector('header').offsetHeight;
-      const targetPosition = target.offsetTop -  headerHeight 
-      - 20;
-      window.scrollTo({
-        top: targetPosition,
-        behavior: 'smooth'
-      });
-    }
+
+    const section = document.querySelector(this.getAttribute('href'));
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
   });
 });
+
